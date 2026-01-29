01f607d2a2fb5ed4cb4eaefd7dce3287078e1add
“Veeresh85” <veeresh.settyb@gmail.com>
2026-01-29 19:18:10 +0100

    Var

--- variable.sh
+++ variable.sh
@@ -1,2 +1,17 @@
 #!bin/bash
 echo "Joindevops"
+
+$Num1=100
+$Num2=200
+$Num3=300
+
+$sum=$(Num1+Num3)
+echo "$sum"
+sleep 20 &
+echo "$user"
+$sub=($Num3-$Num1)
+echo "$sub"
+$Multiply=$Num3x$Num1
+echo "Multiply"
+echo "$$"
+echo "$!"
