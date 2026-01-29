d0809e3bb17e131a12458eab440d80b321bcaa4d
“Veeresh85” <veeresh.settyb@gmail.com>
2026-01-29 19:31:20 +0100

    Var

--- variable.sh
+++ variable.sh
@@ -7,10 +7,10 @@ Num3=300
 Sum=$((Num1+Num3))
 echo "sum is $Sum"
 sleep 20 &
-echo "$user"
-Sub=(($Num3-$Num1))
+echo $user
+Sub=$((Num3-Num1))
 echo "$Sub"
 Multiply=$((Num3xNum1))
 echo "Multiply"
 echo "Current Script PID is $$"
-echo "Background PID$!"
+echo "Background PID $!"
