09a3a939faf0817ac47cb6c6f04f32283052409c
“Veeresh85” <veeresh.settyb@gmail.com>
2026-01-30 19:16:30 +0100

    01

---
+++ variable3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+NUMBER3=300
+NUMBER4=400
+
+SUM=$($NUMBER1+NUMBER2+NUMBER3))
+SUB=(($NUMBER3-$NUMBER1))
+
+echo "Addition of numbers is $SUM"
+
+echo "Substraction of numbers is $SUB"
