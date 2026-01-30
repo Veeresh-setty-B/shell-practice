12e087c8cc8c007e517f37259254e30b975cf238
“Veeresh85” <veeresh.settyb@gmail.com>
2026-01-30 20:30:10 +0100

    01

--- condition.sh
+++ condition.sh
@@ -10,9 +10,9 @@ NUM1=$1
 
 if [ $NUM1 -gt 20 ]; then
 echo "Given number: $NUM1 is greater than 20"
-elif [ $NUM1 -eq 20]
+elif [ $NUM1 -eq 20 ]
 echo "Given number: $NUM1 is equal to 20"
-else
+else 
 echo "Given number: $NUM1 is less than 20"
 fi
 
