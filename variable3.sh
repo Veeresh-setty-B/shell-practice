30df70020e21ea93d21fda4c0ab71d5e84a98d50
“Veeresh85” <veeresh.settyb@gmail.com>
2026-01-30 19:29:29 +0100

    01

--- variable3.sh
+++ variable3.sh
@@ -7,7 +7,17 @@ NUMBER4=400
 
 SUM=$((NUMBER1+NUMBER2+NUMBER3))
 SUB=$((NUMBER3-NUMBER1))
+MUL=$((NUMBER2*NUMBER1))
 
 echo "Addition of numbers is $SUM"
 
 echo "Substraction of numbers is $SUB"
+echo "Multiplications of numbers is $MUL"
+
+##Arrays
+FOOD=("BREAKFAST" "LUNCH" "COFFEE" "DINNER")
+echo "My food are ${FOOD [@]}"
+echo "Morning I eat ${FOOD [0]}"
+echo "Afternoon I eat ${FOOD [1]}"
+echo "Evening I drink ${FOOD [2]}"
+echo "Night I eat ${FOOD [3]}"
