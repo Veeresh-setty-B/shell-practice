72a0ed48fa72149499d71d3105d507789d55c6bf
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-01 11:51:16 +0100

    push

--- loops.sh
+++ loops.sh
@@ -2,13 +2,16 @@
 
 USERID=$(ip -u)
 
-if ($USERID -ne 0); then
+if [ $USERID -ne 0 ]; then
     echo "Run the command with ROOt user"
+    exit 1
 else
     echo "Installing"
 fi
-VALIDATE { if ($1 -ne 0)
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
     echo "$2: Failure"
+    exit 1
 else
     echo "$2 : Success"
 }
