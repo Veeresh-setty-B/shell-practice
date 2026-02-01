052e707f9b125a54aefc28581794ffa25ae4240e
“Veeresh85” <veeresh.settyb@gmail.com>
2026-02-01 11:43:10 +0100

    push

---
+++ loops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(ip -u)
+
+if ($USERID -ne 0)
+    echo "Run the command with ROOt user"
+else
+    echo "Installing"
+fi
+VALIDATE { if ($1 -ne 0)
+    echo "$2: Failure"
+else
+    echo "$2 : Success"
+}
+for package in $@
+do
+    dnf install $@ -y
+    VALIDATE $? "Installing $package"
+done
